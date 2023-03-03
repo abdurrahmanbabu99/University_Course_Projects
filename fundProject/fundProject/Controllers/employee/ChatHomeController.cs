6c26028b2b8de3af50d4cb8da9c75de6276a773b
[abdur1939] <[abdur.info24@gmail.com]>
2023-03-03 19:11:03 +0600

    Added here

---
+++ fundProject/fundProject/Controllers/employee/ChatHomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fundProject.Controllers.employee
+{
+    public class ChatHomeController : Controller
+    {
+        // GET: ChatHome
+        public ActionResult Chat()
+        {
+            if (Session["username"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View("~/Views/employee/ChatHome/Chat.cshtml");
+        }
+    }
+}
